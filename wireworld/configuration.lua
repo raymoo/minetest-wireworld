1c8991ab75a8dc47e9e6ad390606603f19b1e66c
raymoo <uguu@installgentoo.com>
2015-04-15 15:29:49 -0700

    ABMs can't go below 1 second intervals anyway

--- wireworld/configuration.lua
+++ wireworld/configuration.lua
@@ -2,12 +2,8 @@
 
 
 -- TIMESTEP is the simulation timestep for wireworld.
---
--- Don't set it too low, or you might get odd behavior. Maybe I am only saying
--- this because I don't quite understand how guaranteed the timings of
--- minetest.after and abms are.
 
-TIMESTEP = 0.5
+TIMESTEP = 1
 
 Config = {}
 
